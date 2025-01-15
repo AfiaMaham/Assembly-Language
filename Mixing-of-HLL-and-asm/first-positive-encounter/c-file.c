0fa2eef9d1375a72d9f68f942b2f9cbc318ae6eb
AfiaMaham <afiamaham08@gmail.com>
2025-01-15 12:36:49 +0500

    c-file

---
+++ Mixing-of-HLL-and-asm/first-positive-encounter/c-file.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+//extern "C" void __stdcall asmfunc(void);
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void __stdcall asmfunc(int* arr);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+int main() {
+    system("cls");
+    int arr[5] = {1, 2, 3, 4, 5};
+    int len = sizeof (arr)/sizeof (arr[0]);
+    printf("Array: ");
+    for(int i = 0; i < len; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+getch();
+
+    
+    asmfunc(arr); //assembly proc calling
+   
+   getch();
+    
+    printf("After calling asmfunc:\n");
+    printf("array: ");
+    for(int i = 0; i < len; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+
+
+    
+  
+    
+    return 0;
+}
